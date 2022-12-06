f201c47cf415220bd5e304dfe3e8e2c63a7ed761
poojakumari11228 <poojakumari11228@gmail.com>
2022-12-05 23:12:14 -0600

    Lab4(Prob C & D)-Polymorphism,abstract class,sequence diagram

---
+++ Lab4-C/prob3/Order.java
@@ -0,0 +1,28 @@
+package lesson4.labsolns.prob1;
+
+import java.time.LocalDate;
+
+public class Order {
+	private String orderNo;
+	private LocalDate orderDate;
+	private double orderAmount;
+	public Order(String orderNo, LocalDate orderDate, double orderAmount) {
+		this.orderDate = orderDate;
+		this.orderAmount = orderAmount;
+		this.orderNo = orderNo;
+	}
+
+	public void setOrderNo(String s) {
+		orderNo = s;
+	}
+	public String getOrderNo() {
+		return orderNo;
+	}
+	public LocalDate getOrderDate() {
+		return orderDate;
+	}
+	public double getOrderAmount() {
+		return orderAmount;
+	}
+	
+}
